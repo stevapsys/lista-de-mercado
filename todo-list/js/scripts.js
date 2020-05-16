2e94109048c0fb70dae8505bbced5fddf638a866
stevapsys <stephanievapsys@gmail.com>
2020-05-16 12:56:26 -0300

    adicionando o opção de check

--- todo-list/js/scripts.js
+++ todo-list/js/scripts.js
@@ -13,7 +13,13 @@ function addTodo (text) {
 
     //Referencia UL no HTML
     const list = document.querySelector('.js-todo-list'); 
-    list.insertAdjacentHTML('beforeend', `<li class="todo-item" data-key="${todo.id}"> 
+    //element.insertAdjacentHTML serve para inserir um texto HTML dentro do JS
+    //beforeend adiciona o texto no final do elemento
+    //data-key serve como um idenficador único do elemento, assim cada tarefa tem um id
+    list.insertAdjacentHTML('beforeend', 
+    `<li class="todo-item" data-key="${todo.id}"> 
+        <input id="${todo.id}" type="checkbox" /> 
+        <label for="${todo.id}" class="tick js-tick"> </label>
         <span>${todo.text}</span> 
         <button class="delete-todo js-delete-todo">
             <svg>
@@ -22,6 +28,19 @@ function addTodo (text) {
         </button> 
 
     </li>`)
+}
+
+//função para checar o to-do 
+function checkTodo(key) {
+    const index = todoItems.findIndex(item => item.id === Number(key));
+    todoItems[index].checked = !todoItems[index].checked;
+
+    const item = document.querySelector(`[data-key='${key}']`);
+    if(todoItems[index].checked){
+        item.classList.add('done');
+    }else {
+        item.classList.remove('done');
+    }
 }
 
 //verifica o evento forumlário e o valor input
@@ -39,6 +58,21 @@ form.addEventListener('submit', event => {
     }
 
 })
+
+//verifica o click e item da lista para checar
+
+
+const list = document.querySelector('.js-todo-list');
+list.addEventListener('click', event => {
+    //target pega todas as ações que o usuário clica 
+    //contains é para achar uma classe
+    if(event.target.classList.contains('js-tick')){
+        const itemKey = event.target.parentElement.dataset.key; 
+        checkTodo(itemKey);
+    }
+})
+
+
 
 
 
